0a59fd395646da8ac2e89de0ab685cdd1e264de4
강희 정 <vesselofgod@naver.com>
2020-10-02 09:22:13 +0000

    index.js 파일 수정

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
  * @format
  */
 
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
