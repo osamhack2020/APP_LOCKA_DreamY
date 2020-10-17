3d2c59ec945009190694fe63d2c768b2b9dfc2d9
강희 정 <vesselofgod@naver.com>
2020-10-17 05:48:46 +0000

    (Lobby) 시계 기능 추가

--- App.js
+++ App.js
@@ -247,6 +247,28 @@ class LobbyScreen extends React.Component {
     header: null ,
   };
 
+  constructor(props){
+    super(props)
+    this.state = {d: new Date()}
+  }
+
+  componentDidMount() { // Clockcmp 컴포넌트가 불러올때마다 1초씩 this.Change()를 부른다 
+    this.timeID = setInterval(
+        () => this.Change(),
+        1000
+    )
+  }
+
+  componentWillUnmount(){ //종료되면 반복하는것도 클리어시키기
+    clearInterval(this.timeID)
+  }
+
+  Change = () => {  //시계 구현
+    this.setState({
+        d : new Date(),
+    })
+  }
+
   render(){
     return(
       <View style={styles.newContainer}>
@@ -261,11 +283,13 @@ class LobbyScreen extends React.Component {
             </TouchableOpacity>
           </View>
           <View style={{flex: 0.7}}/>
-          <View 
-          // 이 View에 시계 넣어주시면 됩니다.-----------------------------------
-          style={{flex: 1.1}}
-          //-------------------------------------------------------------------
-          />
+          <View style={{flex: 1.1}}>
+            <Text 
+              // 시계넣는공간
+              style={{fontWeight: 'bold', fontSize: 15, color: 'white',}}>
+                {this.state.d.getHours()}:{this.state.d.getMinutes()}: {this.state.d.getSeconds()}
+            </Text>
+          </View>
           <View style={{flex: 0.7}}/>
           <View style={styles.codeSec}>
             <View style={styles.curstatus}>
