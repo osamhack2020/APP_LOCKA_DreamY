e8b6e165ac19abcd3e1b67bd1ab02832ea53702f
강희 정 <vesselofgod@naver.com>
2020-10-24 07:32:33 +0000

    해제여부만 확인

--- App.js
+++ App.js
@@ -471,67 +471,70 @@ class LobbyScreen extends React.Component {
     }
   }
 
-clockrender = (dayofweek) => {
-  //{String(this.state.d.getHours()).padStart(2, "0")}:{String(this.state.d.getMinutes()).padStart(2, "0")}:{String(this.state.d.getSeconds()).padStart(2, "0")}
-  var clockText = "";
-  lockedCondition = NativeModules.Block.checkBlockState();
-  pauseLockState = NativeModules.Block.getpauseLock();
-  if(pauseLockState){
-    clockText = "완전해제 상태입니다."
-    String(this.state.d.getHours()).padStart(2, "0")
-  }
-  else if(( (dayofweek==5 && this.state.d.getHours()<=21) || dayofweek==6 || (dayofweek==0 && this.state.d.getHours()<9) )  && lockedCondition==true){
-    if(12<this.state.d.getHours()<=24){
-      //저녁시간
-      var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),21, 0)
-      end.setSeconds(date.getSeconds() + 41400);
+  clockrender = (dayofweek) => {
+    //{String(this.state.d.getHours()).padStart(2, "0")}:{String(this.state.d.getMinutes()).padStart(2, "0")}:{String(this.state.d.getSeconds()).padStart(2, "0")}
+    var clockText = "";
+    lockedCondition = NativeModules.Block.checkBlockState();
+    pauseLockState = NativeModules.Block.getpauseLock();
+    if(pauseLockState){
+      clockText = "완전해제 상태입니다."
+      String(this.state.d.getHours()).padStart(2, "0")
+    }/*
+    else if(( (dayofweek==5 && this.state.d.getHours()<=21) || dayofweek==6 || (dayofweek==0 && this.state.d.getHours()<9) )  && lockedCondition==true){
+      if(12<this.state.d.getHours()<=24){
+        //저녁시간
+        var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),21, 0)
+        end.setSeconds(date.getSeconds() + 41400);
+        var betweenTime = end - this.state.d;
+        var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
+        var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
+        var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
+        clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
+      }
+      else{
+        //오전시간
+        var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),8, 30)
+        var betweenTime = end - this.state.d;
+        var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
+        var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
+        var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
+        clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
+      }
+    }
+    else if( !( (dayofweek==5 && this.state.d.getHours()<=21) || dayofweek==6 || (dayofweek==0 && this.state.d.getHours()<9) ) && lockedCondition==true){
+      //평일 잠금해제까지 남은시간.
+      if(21<=this.state.d.getHours()<=24){
+        //저녁시간
+        var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate()+1, 18, 0)
+        var betweenTime = end - this.state.d;
+        var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
+        var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
+        var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
+        clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
+      }
+      else{
+        //오전시간
+        var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),18, 0)
+        var betweenTime = end - this.state.d;
+        var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
+        var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
+        var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
+        clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
+      }
+    }
+    else if(lockedCondition==false){
+      var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(), 21, 0)
       var betweenTime = end - this.state.d;
       var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
       var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
       var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
-      clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
-    }
+      clockText = "잠금까지 "+Hours+":"+Minutes+":"+Seconds;
+    }*/
     else{
-      //오전시간
-      var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),8, 30)
-      var betweenTime = end - this.state.d;
-      var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
-      var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
-      var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
-      clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
+      clockText = String(this.state.d.getHours()).padStart(2, "0")+":"+String(this.state.d.getMinutes()).padStart(2, "0")+":"+String(this.state.d.getSeconds()).padStart(2, "0")
     }
+    return clockText;
   }
-  else if( !( (dayofweek==5 && this.state.d.getHours()<=21) || dayofweek==6 || (dayofweek==0 && this.state.d.getHours()<9) ) && lockedCondition==true){
-    //평일 잠금해제까지 남은시간.
-    if(21<=this.state.d.getHours()<=24){
-      //저녁시간
-      var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate()+1, 18, 0)
-      var betweenTime = end - this.state.d;
-      var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
-      var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
-      var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
-      clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
-    }
-    else{
-      //오전시간
-      var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(),18, 0)
-      var betweenTime = end - this.state.d;
-      var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
-      var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
-      var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
-      clockText = "해제까지 "+Hours+":"+Minutes+":"+Seconds;
-    }
-  }
-  else if(lockedCondition==false){
-    var end = newDate(this.state.d.getFullYear(),this.state.d.getMonth(),this.state.d.getDate(), 21, 0)
-    var betweenTime = end - this.state.d;
-    var Hours = String(Math.floor((betweenTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, "0");
-    var Minutes = String(Math.floor((betweenTime % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, "0");
-    var Seconds = String(Math.floor((betweenTime % (1000 * 60)) / 1000)).padStart(2, "0");
-    clockText = "잠금까지 "+Hours+":"+Minutes+":"+Seconds;
-  }
-  return clockText;
-}
 
   render(){
     return(
