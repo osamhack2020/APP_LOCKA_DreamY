fa8a9969410d3563f88440bb18efa55270d3ae59
강희 정 <vesselofgod@naver.com>
2020-10-25 06:10:37 +0000

    prop을 이용해서 확인

--- App.js
+++ App.js
@@ -773,7 +773,11 @@ class CalcScreen extends React.Component {
       ]
     });
     if (selectedItem) {
-      ToastExample.show(typeof selectedItem[id], ToastExample.SHORT);
+      var resultText ="";
+      for(prop in selectedItem)  {
+        resultText += String(prop) + " : "  + String(selectedItem[prop])
+      }
+      ToastExample.show(resultText, ToastExample.SHORT);
       (selectedItem) => {this.setState({corporalPromotion:selectedItem[id]})}
       // when negative button is clicked, selectedItem is not present, so it doesn't get here
       //console.log('You picked:', selectedItem);
