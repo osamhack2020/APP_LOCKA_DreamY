7605293c890a48e5b165aa0a88e5b29b5a29d0b9
강희 정 <vesselofgod@naver.com>
2020-10-25 02:11:21 +0000

    selectedItem 객체출력

--- App.js
+++ App.js
@@ -772,7 +772,7 @@ class CalcScreen extends React.Component {
       ]
     });
     if (selectedItem) {
-      ToastExample.show(String(selectedId), ToastExample.SHORT);
+      ToastExample.show(selectedItem, ToastExample.SHORT);
       (selectedId) => {this.setState({corporalPromotion:selectedId})}
       // when negative button is clicked, selectedItem is not present, so it doesn't get here
       //console.log('You picked:', selectedItem);
