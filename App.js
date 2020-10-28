5fa6b233ec31c683556d576015fbabe5294afde0
강희 정 <vesselofgod@naver.com>
2020-10-28 10:02:53 +0000

    View를 통해 Picker 디자인

--- App.js
+++ App.js
@@ -938,32 +938,35 @@ class CalcScreen extends React.Component {
               <View style={{flexDirection: 'column', alignSelf: 'center'}}>
                 <Button title="적금입력" onPress={this.showDialogAndroid} />
               </View>
-                <View style={{alignSelf: 'center', flexDirection: 'row'}}>
-                  <Picker
-                    style={{ height: 50, width: 200, color: "black", backgroundColor:'#fff'}}
-                    onValueChange={(itemValue, itemIndex) =>
-                      this.setState({sgtPromotion: Number(itemValue)})} 
-                    selectedValue={String(this.state.sgtPromotion)}
-                  >
-                    <Picker.Item label="(상병)2달 진급누락" value = "-2"/>
-                    <Picker.Item label="(상병)1달 진급누락" value="-1" />
-                    <Picker.Item label="(상병)정상진급" value="0" />
-                    <Picker.Item label="(상병)1달 조기진급" value="1" />
-                    <Picker.Item label="(상병)2달 조기진급" value="2" />
-                  </Picker>
-                  <Picker
-                    style={{height: 50, width: 200, color: 'black' ,backgroundColor:'#fff'}}
-                    //textStyle={{ fontSize:16, color: 'white'}}
-                    onValueChange={(itemValue, itemIndex) =>
-                      this.setState({corporalPromotion: Number(itemValue)})}
-                    selectedValue={String(this.state.corporalPromotion)}
-                  >
-                    <Picker.Item label="(병장)1달 진급누락" value="-1" />
-                    <Picker.Item label="(병장)정상진급" value="0" />
-                    <Picker.Item label="(병장)1달 조기진급" value="1" />
-                  </Picker>
+                <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+                  <View style={{alignSelf: 'center', flexDirection: 'row', margin: 5,  padding: 5}}>
+                    <Picker
+                      style={{ height: 50, width: 190, color: "black", backgroundColor:'#A8DDA8'}}
+                      onValueChange={(itemValue, itemIndex) =>
+                        this.setState({sgtPromotion: Number(itemValue)})} 
+                      selectedValue={String(this.state.sgtPromotion)}
+                    >
+                      <Picker.Item label="(상병)2달 진급누락" value = "-2"/>
+                      <Picker.Item label="(상병)1달 진급누락" value="-1" />
+                      <Picker.Item label="(상병)정상진급" value="0" />
+                      <Picker.Item label="(상병)1달 조기진급" value="1" />
+                      <Picker.Item label="(상병)2달 조기진급" value="2" />
+                    </Picker>
+                  </View>
+                  <View style={{alignSelf: 'center', flexDirection: 'row', margin: 5,  padding: 5,}}>
+                    <Picker
+                      style={{height: 50, width: 190, color: 'black' ,backgroundColor:'#EFFAD3'}}
+                      //textStyle={{ fontSize:16, color: 'white'}}
+                      onValueChange={(itemValue, itemIndex) =>
+                        this.setState({corporalPromotion: Number(itemValue)})}
+                      selectedValue={String(this.state.corporalPromotion)}
+                    >
+                      <Picker.Item label="(병장)1달 진급누락" value="-1" />
+                      <Picker.Item label="(병장)정상진급" value="0" />
+                      <Picker.Item label="(병장)1달 조기진급" value="1" />
+                    </Picker>
+                  </View>
                 </View>
-
                 <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                 {
                   this.state.clicked
